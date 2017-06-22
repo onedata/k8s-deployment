2c2f86bf237eca54c2e8a485304f26ce3f318ee4
Michal Orzechowski <orzechowski.michal@gmail.com>
2017-06-22 20:10:02 +0200

    added --wait and increased --timout options to helm install

--- landscapes/develop/deploy.sh
+++ landscapes/develop/deploy.sh
@@ -3,4 +3,4 @@
 # This file specifies a chart, chart version, release name, and a file from where take variables from
 echo helm install --dry-run --debug --namespace $NAMESPACE -n $RELEASE_NAME -f /root/landscapes/develop/cross-support-job-3p.yaml onedata/cross-support-job-3p
 cat landscapes/develop/cross-support-job-3p.yaml
-helm install --debug --namespace $NAMESPACE -n $RELEASE_NAME -f /root/landscapes/develop/cross-support-job-3p.yaml onedata/cross-support-job-3p
+helm install --wait --timeout 480 --debug --namespace $NAMESPACE -n $RELEASE_NAME -f /root/landscapes/develop/cross-support-job-3p.yaml onedata/cross-support-job-3p
