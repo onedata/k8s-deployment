d1786e82bf8d09b3f95d81af8e54ea8b3cb4520e
Michal Orzechowski <orzechowski.michal@gmail.com>
2017-06-22 14:41:14 +0200

    not working with volumes showcase of using docker-compose container instead of docker-compose binary

--- run.sh
+++ run.sh
@@ -2,13 +2,28 @@
 
 if [[ -z $KUBE_CONFIG ]] ; then KUBE_CONFIG=~/.kube/config ; fi
 
-export KUBE_CONFIG="$KUBE_CONFIG"
-export NAMESPACE=$NAMESPACE
-export RELEASE_NAME=$RELEASE_NAME
-export OZ_IMAGE=$OZ_IMAGE
-export OP_IMAGE=$OP_IMAGE
-export OC_IMAGE=$OC_IMAGE
-export CLI_IMAGE=$CLI_IMAGE
+docker run --rm \
+    -v $PWD/docker:/docker:ro \
+    -e KUBE_CONFIG="$KUBE_CONFIG" \
+    -e NAMESPACE=$NAMESPACE \
+    -e RELEASE_NAME=$RELEASE_NAME \
+    -e OZ_IMAGE=$OZ_IMAGE \
+    -e OP_IMAGE=$OP_IMAGE \
+    -e OC_IMAGE=$OC_IMAGE \
+    -e CLI_IMAGE=$CLI_IMAGE \
+    docker/compose:1.14.0 -f docker/docker-compose.yaml config
 
-docker-compose -f docker/docker-compose.yaml config
-docker-compose -f docker/docker-compose.yaml up --remove-orphans
+docker run \
+    -v "/var/run/docker.sock:/var/run/docker.sock" \
+    -v $PWD/docker:/docker \
+    -v $PWD/landscapes:/landscapes \
+    -e KUBE_CONFIG="$KUBE_CONFIG" \
+    -e NAMESPACE=$NAMESPACE \
+    -e RELEASE_NAME=$RELEASE_NAME \
+    -e OZ_IMAGE=$OZ_IMAGE \
+    -e OP_IMAGE=$OP_IMAGE \
+    -e OC_IMAGE=$OC_IMAGE \
+    -e CLI_IMAGE=$CLI_IMAGE \
+    --entrypoint sh \
+    -it \
+    docker/compose:1.14.0 # -f docker/docker-compose.yaml up --remove-orphans
