c75b639c7389010d6c72ef7207a5cad756827c4f
Michal Orzechowski <orzechowski.michal@gmail.com>
2017-06-27 09:41:40 +0200

    added explicity version information about the chart that will be used

--- landscapes/develop/deploy.sh
+++ landscapes/develop/deploy.sh
@@ -8,5 +8,5 @@ if [[ $RELEASE_NAME != "" ]]; then
     RELEASE_NAME="-n $RELEASE_NAME"
 fi
 
-echo helm install --dry-run --debug --namespace $NAMESPACE $RELEASE_NAME -f /root/landscapes/develop/cross-support-job-3p.yaml onedata/cross-support-job-3p
-helm install --wait --timeout 480 --debug --namespace $NAMESPACE $RELEASE_NAME -f /root/landscapes/develop/cross-support-job-3p.yaml onedata/cross-support-job-3p
+echo helm install --dry-run --debug --namespace $NAMESPACE $RELEASE_NAME -f /root/landscapes/develop/cross-support-job-3p.yaml onedata/cross-support-job-3p --version 0.2.0
+helm install --wait --timeout 480 --debug --namespace $NAMESPACE $RELEASE_NAME -f /root/landscapes/develop/cross-support-job-3p.yaml onedata/cross-support-job-3p --version 0.2.0
