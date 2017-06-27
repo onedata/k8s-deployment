0f0ac4e834f18f177afa68e772c32a97f2ed59b4
Michal Orzechowski <orzechowski.michal@gmail.com>
2017-06-27 08:31:38 +0200

    added explicity version information about the chart that will be used

--- landscapes/develop/deploy.sh
+++ landscapes/develop/deploy.sh
@@ -2,7 +2,8 @@
 
 # This file specifies a chart, chart version, release name, and a file from where take variables from
 
-chart_to_deploy="local/cross-support-job-3p"
+chart_to_deploy="onedata/cross-support-job-3p"
+version="0.2.1"
 
 if [[ $release_name != "" ]]; then
     release_name="-n $release_name"
@@ -16,8 +17,8 @@ if [[ $helm_debug != "" ]]; then
     helm_debug="--debug"
 fi
 
-echo helm install --wait $helm_dry_run --timeout 480 $helm_debug --namespace $namespace $release_name -f ~/landscapes/$landscape/landscape.yaml "$chart_to_deploy"
+echo helm install --wait $helm_dry_run --timeout 480 $helm_debug --namespace $namespace $release_name -f ~/landscapes/$landscape/landscape.yaml "$chart_to_deploy" --version $version
 
 cat ~/landscapes/$landscape/landscape.yaml
 echo ""
-helm install --wait $helm_dry_run --timeout 480 $helm_debug --namespace $namespace $release_name -f ~/landscapes/$landscape/landscape.yaml "$chart_to_deploy"
+helm install --wait $helm_dry_run --timeout 480 $helm_debug --namespace $namespace $release_name -f ~/landscapes/$landscape/landscape.yaml "$chart_to_deploy" --version $version
