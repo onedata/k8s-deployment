b151351ff8363966f37cca66428378aaf8f62738
groundnuty <groundnuty@gmail.com>
2018-04-09 11:22:43 +0200

    added 3p ladnscape without s3

---
+++ landscapes/onedata-3p-wo-S3/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# This file specifies a chart, chart version, release name, and a file from where take variables from
+
+chart_to_deploy="onedata/cross-support-job-3p"
+version="0.2.10"
+
+if [[ $release_name != "" ]]; then
+    release_name="-n $release_name"
+fi
+
+if [[ $helm_dry_run != "" ]]; then
+    helm_dry_run="--dry-run"
+fi
+
+if [[ $helm_debug != "" ]]; then
+    helm_debug="--debug"
+fi
+
+echo helm install $helm_dry_run --timeout 480 $helm_debug --namespace $namespace $release_name -f ~/landscapes/$landscape/landscape.yaml "$chart_to_deploy" --version $version
+cat ~/landscapes/$landscape/landscape.yaml
+helm install $helm_dry_run --timeout 480 $helm_debug --namespace $namespace $release_name -f ~/landscapes/$landscape/landscape.yaml "$chart_to_deploy" --version $version
